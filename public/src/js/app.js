3f6984c32c27a7ca619d7a3bc13377212600c2f8
guanocoder <25417330+guanocoder@users.noreply.github.com>
2022-01-04 19:42:02 +0300

    deferring installation prompt to some later event in the app execution

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,14 @@
+let deferredPrompt = null;
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').then(function() {
         console.log("Service Worker registered!");
     });
 }
+
+window.addEventListener("beforeinstallprompt", function(event) {
+    console.log("prevented on 'beforeinstallprompt'");
+    deferredPrompt = event;
+    event.preventDefault();
+    return false;
+})
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -4,6 +4,22 @@ var closeCreatePostModalButton = document.querySelector('#close-create-post-moda
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
+
+  if (deferredPrompt) {
+    console.log('found deferred prompt, now firing the event');
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then(function(choiceResult) {
+        console.log("user picked", choiceResult);
+        if (choiceResult.outcome === 'dismissed') {
+            console.log("User dismissed app installation");
+        } else {
+            console.log("User added app to home screen");
+        }
+    });
+    deferredPrompt = null;
+  } else {
+    console.log('did not find deferred prompt event');
+  }
 }
 
 function closeCreatePostModal() {
