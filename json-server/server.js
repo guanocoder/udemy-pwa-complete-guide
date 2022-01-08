b07ae09f92c492b5f3d9c1c2af48f9af21f1175f
guanocoder <25417330+guanocoder@users.noreply.github.com>
2022-01-08 18:17:48 +0300

    placed data server and image server to separate paths

--- json-server/server.js
+++ json-server/server.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const jsonServer = require('json-server');
 
 const PORT = 5000;
@@ -7,16 +8,16 @@ const server = jsonServer.create();
 const router = jsonServer.router(SOURCE_FILE);
 const middlewares = jsonServer.defaults();
 
-// Добавить заголовки что бы браузер не переживал по поводу CORS
-// (запуск с флажком --no-cors почему-то не дал должного результата)
 server.use('/*', function(request, response, next) {
     response.header("Access-Control-Allow-Origin", "*");
     response.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
 
-server.use(middlewares);
-server.use(router);
+server.use('/images', express.static('images'));
+
+server.use('/data', middlewares);
+server.use('/data', router);
 server.listen(PORT, () => {
     console.log(`JSON Server serving '${SOURCE_FILE}' on port:${PORT}`);
 });
