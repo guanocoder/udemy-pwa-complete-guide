bb65226eb5eff8e2bb8f0c0f7c83322bc7ffc21d
guanocoder <25417330+guanocoder@users.noreply.github.com>
2022-01-09 22:36:59 +0300

    reading data from API

--- front/public/src/js/feed.js
+++ front/public/src/js/feed.js
@@ -45,23 +45,23 @@ function onCardSaveButtonClick(event) {
 }
 */
 
-function createCard() {
+function createCard(data) {
     var cardWrapper = document.createElement('div');
     cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
     var cardTitle = document.createElement('div');
     cardTitle.className = 'mdl-card__title';
-    cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
+    cardTitle.style.backgroundImage = `url("${data.image}")`;
     cardTitle.style.backgroundSize = 'cover';
     cardTitle.style.height = '180px';
     cardWrapper.appendChild(cardTitle);
     var cardTitleTextElement = document.createElement('h2');
     cardTitleTextElement.className = 'mdl-card__title-text';
-    cardTitleTextElement.textContent = 'San Francisco Trip';
+    cardTitleTextElement.textContent = data.title;
     cardTitleTextElement.style.color = 'white';
     cardTitle.appendChild(cardTitleTextElement);
     var cardSupportingText = document.createElement('div');
     cardSupportingText.className = 'mdl-card__supporting-text';
-    cardSupportingText.textContent = 'In San Francisco';
+    cardSupportingText.textContent = data.location;
     cardSupportingText.style.textAlign = 'center';
     // var cardSaveButton = document.createElement('button');
     // cardSaveButton.textContent = 'Save';
@@ -71,11 +71,24 @@ function createCard() {
     componentHandler.upgradeElement(cardWrapper);
     sharedMomentsArea.appendChild(cardWrapper);
 }
-  
-fetch('https://httpbin.org/get')
-    .then(function(res) {
+
+function loadPosts(postsData) {
+    if (Array.isArray(postsData)) {
+        for (let i = 0; i < postsData.Data.length; data++) {
+            createCard(postsData[i]);
+        }
+    } else {
+        createCard(postsData);
+    }
+}
+
+getPost('first-post');
+
+function getPost(postId) {
+    return fetch(`http://localhost:5000/data/posts/${postId}`).then(function(res) {
         return res.json();
-    })
-    .then(function(data) {
-        createCard();
-    });
+    }).then(function(data) {
+        console.log("|-> fetched the following data", data);
+        loadPosts(data);
+    });    
+}
